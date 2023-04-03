140f57b021a05be5683e30bd9961ba291eb8be5e
Your Name <you@example.com>
2023-04-03 02:10:56 +0200

    up

--- gpt.py
+++ gpt.py
@@ -1,54 +1,60 @@
 import subprocess
-import pulsectl
-from tkinter import *
+import redis
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-# the Spotify track URIs to play
-track_uris = ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:3eZEiV7w8Q0MVjVXRvvZtX', 'spotify:track:3qXpX7tK4fNkwJF8Y4WtSA']
+# Initialize Redis connection
+redis_conn = redis.Redis(host='localhost', port=6379, db=0)
 
-# start Raspotify as a subprocess
-raspotify_process = subprocess.Popen(['raspotify', '--backend', 'pipe', '--onevent', 'python /home/mi/hackp0d/gpt.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+# Initialize Spotify connection
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read"))
 
-# create a PulseAudio client
-pulse = pulsectl.Pulse('raspotify-client')
+# Main menu function
+def menu(items, title):
+    while True:
+        print("\033[2J")  # Clear screen
+        print("\033[1;32;40m", title, "\033[0m\n")  # Header
+        for i, item in enumerate(items):
+            if i == 0:
+                print("\033[1;32;40m>\033[0m", item)
+            else:
+                print(" ", item)
+        keypress = input("\nUse arrow keys to scroll, press enter to select\n")
+        if keypress == "\x1b[A":  # Up arrow
+            items = items[-1:] + items[:-1]
+        elif keypress == "\x1b[B":  # Down arrow
+            items = items[1:] + items[:1]
+        elif keypress == "\r":  # Enter key
+            return items[0]
 
-# set the default sink to the Bluetooth speaker
-default_sink = pulse.sink_list()[1] # adjust index as needed
-pulse.default_set(default_sink)
+# Get user's saved albums from Spotify API
+albums = sp.current_user_saved_albums(limit=50)["items"]
 
-# create the Tkinter window
-root = Tk()
-root.title('Raspotify')
+# Add album names to Redis database
+for album in albums:
+    redis_conn.lpush("albums", album["album"]["name"])
 
-# create a header label
-header_label = Label(root, text='Raspotify', font=('Courier', 16), fg='green', bg='black')
-header_label.pack(side=TOP, fill=X)
+# Main loop
+while True:
+    menu_items = ["albums", "artists", "playlists", "settings", "power off"]
+    menu_item = menu(menu_items, "iPod Menu")
+    if menu_item == "albums":
+        album_names = redis_conn.lrange("albums", 0, -1)
+        album_choice = menu(album_names, "Albums")
+        if album_choice is not None:
+            album_tracks = sp.album_tracks(album_choice.decode("utf-8"))
+            track_names = [track["name"] for track in album_tracks["items"]]
+            track_choice = menu(track_names, album_choice.decode("utf-8"))
+            if track_choice is not None:
+                track_uri = album_tracks["items"][track_choice]["uri"]
+                subprocess.call(["raspotify", "-p", track_uri])
+    elif menu_item == "artists":
+        pass  # TODO: Implement artist browsing
+    elif menu_item == "playlists":
+        pass  # TODO: Implement playlist browsing
+    elif menu_item == "settings":
+        pass  # TODO: Implement settings menu
+    elif menu_item == "power off":
+        break
 
-# create a listbox to display the track options
-track_listbox = Listbox(root, font=('Courier', 14), fg='green', bg='black', selectmode=SINGLE, highlightcolor='green', highlightbackground='green')
-for track_uri in track_uris:
-    track_listbox.insert(END, track_uri)
-track_listbox.pack(side=TOP, fill=BOTH, expand=YES)
-
-# create a play button to start the playback
-def play_track():
-    selection = track_listbox.curselection()
-    if selection:
-        track_uri = track_uris[selection[0]]
-        raspotify_process.stdin.write(f'play {track_uri}\n'.encode())
-
-play_button = Button(root, text='Play', font=('Courier', 14), fg='green', bg='black', command=play_track)
-play_button.pack(side=TOP, pady=10)
-
-# create a stop button to stop the playback
-def stop_track():
-    raspotify_process.stdin.write(b'stop\n')
-
-stop_button = Button(root, text='Stop', font=('Courier', 14), fg='green', bg='black', command=stop_track)
-stop_button.pack(side=TOP, pady=10)
-
-# run the Tkinter event loop
-root.mainloop()
-
-# reset the default sink to the internal speakers
-default_sink = pulse.sink_list()[0] # adjust index as needed
-pulse.default_set(default_sink)
+print("Goodbye!")
